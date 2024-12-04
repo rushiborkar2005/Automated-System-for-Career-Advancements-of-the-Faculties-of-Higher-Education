fe76a31f1cbc4d51222955437b305994d09d7217
Mahima-Umak2005 <mahimaumak6868@gmail.com>
2024-12-04 13:11:26 +0530

    Add files via upload

--- Frontend2/Department-HOD/hodEndorse/main.js
+++ Frontend2/Department-HOD/hodEndorse/main.js
@@ -1,11 +1,24 @@
-import { FacultyData } from './data.js';
-import { setupFilters } from './filters.js';
-import { setupTable } from './table.js';
-import { setupChatWidget } from './chat.js';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const facultyData = new FacultyData();
-    setupFilters(facultyData);
-    setupTable(facultyData);
-    setupChatWidget();
-});
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
